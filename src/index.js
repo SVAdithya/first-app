ab2e378e155532fb475889e38f8ad91b1bc578f3
SVAdithya <svadithya93@gmail.com>
2024-11-03 02:27:37 +0530

    Variable Attribute in a Component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import {MyComponent, MyHello } from './App';
+import Owl from './Owl.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
         <MyComponent />
         <MyHello />
+        <Owl/>
     </>
 );
