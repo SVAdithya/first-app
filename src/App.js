83f8f876e9374270a484b78b013faab6a5baad57
SVAdithya <svadithya93@gmail.com>
2024-11-03 02:27:06 +0530

    Multiple components export

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <img src="./logo512.png" className="App-logo" alt="logo" />
-      <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function MyComponent() {
+    return <h1>Hello world</h1>;
 }
 
-export default App;
+function MyHello() {
+    return <p>Hi Adithya</p>;
+}
+
+// Use curly braces for named exports
+export { MyComponent, MyHello };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import {MyComponent, MyHello } from './App';
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
+        <MyComponent />
+        <MyHello />
+    </>
 );
-
-
